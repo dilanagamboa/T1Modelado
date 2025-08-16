4576e1c6ab2e43765fb424bcf0d42dec92608954
Dilana Gamboa <dilana.gamboa2@gmail.com>
2025-08-16 12:38:51 -0600

    first commit

---
+++ prueba1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliotecaElegante
+{
+    //la base 
+    public class Entidad
+    {
+    public int Id { get; set; }
+    public string Nombre { get; set; } = "";
+    }
+
+    //publicación abstracta
+    public abstract class Publicacion : Entidad
+    {
+    public string Autor { get; set; } = "";
+    public int AnioPublicacion { get; set; }
+    public abstract void MostrarInformacion();
+    }
+
+    //clase biblioteca (funciona como gestor principal)
+    public class Biblioteca
+    {
+        private List<Publicacion> publicaciones;
+
+        public Biblioteca()
+        {
+            publicaciones = new List<Publicacion>();
+        }
+
+        public void AgregarPublicacion(Publicacion publicacion)
+        {
+            publicaciones.Add(publicacion);
+        }
+
+        public void ListarPublicaciones()
+        {
+            foreach (var pub in publicaciones)
+            {
+                pub.MostrarInformacion();
+            }
+        }
+    }
+}
